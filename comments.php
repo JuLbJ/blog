39f9b91027e27f365b7f49b325f004612ea2dc24
Justyna Bruniaux <justyna.lipska74@wp.pl>
2016-10-11 15:12:16 +0200

    css added

--- comments.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: justyna_lb
- * Date: 11/10/16
- * Time: 01:47
- */
--- index.php
+++ index.php
@@ -1,7 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+    <link href="style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <h1>Mon super blog !</h1>
+        <p>Dernièrs billets du blog :</p>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: justyna_lb
- * Date: 11/10/16
- * Time: 01:47
- */
+// *******************   1. Connection à la base de donnees
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'justyna24');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+
+// *******************   2. On récupère 5 dérniers billet
+
+$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d%m%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+while ($donnees = $req->fetch()) {
+    ?>
+    <div class="news">
+        <h3>
+            <?php echo htmlspecialchars($donnees['titre']); ?>
+            <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+            <h3>
+
+                <p>
+                    <?php
+                    // *******************   3_1. On affiche le contenu du billet
+                    echo nl2br(htmlspecialchars($donnees['contenu']));
+                    ?>
+                    <br/>
+                    <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a>
+                    </em>
+                </p>
+    </div>
+    <?php
+} // *******************   3_2. Fin de la boucle des billets
+
+$req->closeCursor();
+?>
+</body>
+</html>
+
